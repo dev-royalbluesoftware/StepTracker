a77f6829be46f6803d0ed43ee924e42abf015bd5
jjb-rbs <111300320+dev-royalbluesoftware@users.noreply.github.com>
2024-06-07 19:45:34 -0600

    initial UI

--- StepTracker/ContentView.swift
+++ StepTracker/ContentView.swift
@@ -5,20 +5,69 @@
 //
 // Created by jjbuell-dev
 // Copyright © Royal Blue Software
-// 
+//
 
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                
+                // MARK: - Main VStack
+                
+                VStack(spacing: 20) {
+                    // Card VStack
+                    VStack {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Label("Steps", systemImage: "figure.walk")
+                                    .font(.title3.bold())
+                                    .foregroundStyle(.pink)
+                                
+                                Text("Avg: 10K Steps")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .foregroundStyle(.secondary)
+                        }
+                        .padding(.bottom, 12)
+                        
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 150)
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                    // Card VStack
+                    VStack(alignment: .leading) {
+                        VStack(alignment: .leading) {
+                            Label("Averages", systemImage: "calendar")
+                                .font(.title3.bold())
+                                .foregroundStyle(.pink)
+                            
+                            Text("Last 28 Days")
+                                .font(.caption)
+                                .foregroundStyle(.secondary)
+                        }
+                        .padding(.bottom, 12)
+                        
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 240)
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                }
+            }
+            .padding()
+            .navigationTitle("Dashboard")
         }
-        .padding()
     }
 }
 
